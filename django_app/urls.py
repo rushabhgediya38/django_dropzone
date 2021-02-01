292e9968d0aedd164f0be7e7bf91c064344a3e12
Rushabh Gediya <rushabhgediya38@gmail.com>
2021-02-01 09:05:08 +0530

    Update

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 
+
 urlpatterns = [
     path('', home, name='home'),
     path('post_create/', post_create, name='post_create'),
--- django_app/views.py
+++ django_app/views.py
@@ -16,18 +16,23 @@ def home(request):
 def post_create(request):
     if request.method == 'POST' or request.FILES:
 
-        name1 = request.POST['name']
-        username1 = request.POST['username']
+        name1 = request.POST.get('name')
+        username1 = request.POST.get('username')
 
         files = [request.FILES.get('file[%d]' % i) for i in range(0, len(request.FILES))]
         images = request.FILES.get('file[0]', None)
 
-        for f in files:
-            data = f
-            posttt = post.objects.create(name=name1, username=username1, img=data)
+        if files:
+            for f in files:
+                data = f
+                posttt = post.objects.create(name=name1, username=username1, img=data)
+                posttt.save()
+            # filess = request.FILES.getlist('images')
+            return redirect('/')
+        else:
+            posttt = post.objects.create(name=name1, username=username1)
             posttt.save()
-        # filess = request.FILES.getlist('images')
-        return redirect('/')
+            return redirect('/')
 
     else:
         return render(request, 'post_create.html', {
@@ -50,18 +55,29 @@ def create(request):
 
 def final_post(request):
     if request.method == 'POST' or request.FILES:
-        name11 = request.POST.get('name', False)
-        username11 = request.POST.get('username', False)
-        img12 = request.FILES.get('img1', False)
-        print(img12)
+        name11 = request.POST.get('name')
+        username11 = request.POST.get('username')
+        img12 = request.FILES.get('img1')
 
         files = [request.FILES.get('file[%d]' % i) for i in range(0, len(request.FILES))]
         images = request.FILES.get('file[0]', None)
 
         for f in files:
             data = f
-            posttt = post123.objects.create(name=name11, username=username11, img1=img12, img=data)
+            posttt = post123.objects.create(name=name11, username=username11, img=data, img1=img12)
             posttt.save()
             return redirect('/')
+
+        if img12:
+            posttt = post123.objects.create(name=name11, username=username11, img1=img12)
+            posttt.save()
+            return redirect('/')
+
+        if files:
+            for f in files:
+                data = f
+                posttt = post123.objects.create(name=name11, username=username11, img=data)
+                posttt.save()
+                return redirect('/')
 
     return render(request, 'final_post.html')
